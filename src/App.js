5a01ec3524aa0ec79a5a3df7847f8e909d8a4521
Esther Lartey <estellekai012@gmail.com>
2024-06-28 00:48:46 +0000

    updat categoriesMenu with categoriesMenu component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,35 @@
-import './categories.style.scss';
+import CategoriesMenu from './components/categories-menu/category-menu-container';
 
 const App = () => {
   const categories = [
     {
       id: '1',
       title: 'Hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     },
     {
       id: '2',
       title: 'Jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     },
     {
       id: '3',
       title: 'Sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     },
     {
       id: '4',
       title: 'Womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     },
     {
       id: '5',
       title: 'Mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     },
   ]
   return (
-    <div className="categories-container">
-      {categories.map(({ title, id }) => (
-        <div key={id} className="category-container">
-          <img className="background-image" />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-
-    </div>
+    <CategoriesMenu categories={categories} />
   );
 }
 
---
+++ src/components/categories-menu/category-menu-container.jsx
@@ -0,0 +1,16 @@
+import CategoryItem from '../category-item/category-item-component';
+
+import './categories-menu-container.scss';
+
+const CategoriesMenu = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+
+    </div>
+  )
+}
+
+export default CategoriesMenu;
